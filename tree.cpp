3d476884158f0f4a12794003c22ef3dd12b84543
deepsikhak <deepsikhakar.dak@gmail.com>
2018-05-15 10:26:30 -0700

    etting up all the functions

--- tree.cpp
+++ tree.cpp
@@ -123,13 +123,36 @@ class bst{
 
 	}
 
-	int leafNode(Node* root){
+	void printleafNode(Node* root){
+		if(!root)return;
+		if(!root->getLeft() && !root->getRight())
+			cout<<root->getData()<<" ";
+		if(root->getLeft()) printleafNode(root->getLeft());
+		if(root->getRight())printleafNode(root->getRight());
+	}
+
+	int sumleafNode(Node* root){
+		
+		if(!root)return 0;
+		if(!root->getLeft() && !root->getRight())
+			return root->getData();	
+		if(root->getLeft() && root->getRight()) return sumleafNode(root->getLeft()) + sumleafNode(root->getRight());
+		if(root->getLeft()) return sumleafNode(root->getLeft());
+		if(root->getRight()) return sumleafNode(root->getRight());
 
 	}
 
 	int sumNodes(Node* root){
 		if(root==NULL)return 0;
 		return sumNodes(root->getLeft()) + sumNodes(root->getRight()) + root->getData();
+	}
+
+	int minimumValue(Node* root){
+
+	}
+
+	int inOrderSuccessor(Node* root){
+
 	}
 };
 
@@ -138,13 +161,20 @@ int main(){
 	bst* BT = new bst(100);
 	int choice,newNumber;
 	Node* p;
-	int a,s,sum;
+	int a,s,sum,sumleaf=0;
 	cout<<"A Binary Search Tree created with value 100 as the root";
 	while(1){
 		cout<<"\n0 - To exit"<<endl;
 		cout<<"1 - To Insert Value in the binary search tree"<<endl;
-		cout<<"2 - TO print value in Order"<<endl;
-		cout<<"3 - To print Inorder traversal"<<endl;
+		cout<<"2 - To print Inorder traversal"<<endl;
+		cout<<"3 - To print preOrder traversal"<<endl;
+		cout<<"4 - To print postOrder traversal"<<endl;
+		cout<<"5 - To print levelOrder traversal"<<endl;
+		cout<<"6 - To find maximum Height"<<endl;
+		cout<<"7 - To find minimum height"<<endl;
+		cout<<"8 - To find sum of all the nodes"<<endl;
+		cout<<"9- To print the leaf nodes"<<endl;
+		cout<<"10- To get the sum of all the leaf nodes"<<endl;
 		cin>>choice;
 		switch(choice){
 			case 0:
@@ -179,6 +209,14 @@ int main(){
 			case 8:
 				sum = BT->sumNodes(BT->getRoot());
 				cout<<"The sum of the nodes is "<<sum<<endl;
+				break;
+			case 9:
+				BT->printleafNode(BT->getRoot());
+				break;
+			case 10:
+				sumleaf = BT->sumleafNode(BT->getRoot());
+				cout<<"The sum of the leaf nodes is "<<sumleaf<<endl;
+				break;
 			default:
 				cout<<"Please enter a valid Choice"<<endl;
 				break;
